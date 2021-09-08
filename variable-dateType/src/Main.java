59a07ea4f729da8d80d2bbdcccb8bfdf918ff4cd
thaovt91 <vothao.tin@gmail.com>
2021-09-08 14:43:45 +0700

    update

--- variable-dateType/src/Main.java
+++ variable-dateType/src/Main.java
@@ -3,13 +3,13 @@ public class Main {
         int i = 10 ;
         float f = 20.5f ;
         double d = 20.5 ;
-        boolean b= true ;
+        boolean b = true ;
         char c = 'a';
         String s = "Hà Nội" ;
         System.out.println("i = " + i);
         System.out.println("f = " + f);
         System.out.println("d = "+ d);
-        System.out.println("b = "+ b);
+        System.out.println("b = "+ b );
         System.out.println("c = " + c);
         System.out.println("s = " +  s);
     }
