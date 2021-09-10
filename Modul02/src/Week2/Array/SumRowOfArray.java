c55c52df62b94e1d0b07a392a3cbb862cf966613
thaovt91 <vothao.tin@gmail.com>
2021-09-10 20:54:04 +0700

    up

--- Modul02/src/Week2/Array/SumRowOfArray.java
+++ Modul02/src/Week2/Array/SumRowOfArray.java
@@ -2,8 +2,9 @@ package Week2.Array;
 
 import java.util.Scanner;
 
-public class MaxArrayOfArray {
+public class SumRowOfArray {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int row, column;
         System.out.print("Nhập kích thước mảng 2 chiều : ");
@@ -14,33 +15,31 @@ public class MaxArrayOfArray {
         System.out.println("Nhập mảng 2 chiều có kích thước là '" + row + "' , độ dài của mảng phần tử là '" + column + "' !");
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                System.out.print("Nhập phần tử " + j + " của mảng Array[" + i +"] : ");
+                System.out.print("Nhập phần tử " + j + " của mảng Array[" + i + "] : ");
                 arr[i][j] = scanner.nextInt();
             }
         }
         System.out.println("Mảng 2 chiều nhận được là : ");
         for (int i = 0; i < arr.length; i++) {
             System.out.print("[ ");
-            for (int j = 0; j < arr[i].length-1; j++) {
-                System.out.print(arr[i][j] + ", " );
+            for (int j = 0; j < arr[i].length - 1; j++) {
+                System.out.print(arr[i][j] + ", ");
             }
-            System.out.println(arr[i][arr[i].length -1] + " ] ");
+            System.out.println(arr[i][arr[i].length - 1] + " ] ");
         }
-        int maxArr = maxArray(arr[0]);
-        for (int i =1 ; i < arr.length ; i ++ ){
-            if (maxArr < maxArray(arr[i])){
-                maxArr = maxArray(arr[i]) ;
+        int col, sum = 0;
+        do {
+            System.out.print("Nhập cột cần tính tổng : ");
+            col = scanner.nextInt();
+            if ((col < 0) || (col > column)) {
+                System.out.println("Cột không tồn tại trong mảng");
             }
+        } while ((col < 0) || (col > column));
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i][col];
         }
-        System.out.print("Phần tử lớn nhất của mảng 2 chiều đã cho là : " + maxArr);
-    }
-    public static int maxArray(int[]array){
-        int max = array[0];
-        for (int i = 1; i< array.length ; i++){
-            if (max < array[i]) {
-                max = array[i] ;
-            }
-        }
-        return  max ;
+        System.out.println("Tổng các số tại cột '"+ col+"' của mảng là : " + sum);
+
     }
 }
