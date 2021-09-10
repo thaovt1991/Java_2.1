5370b70fe3c4b7b21b58863f121299640854d9ee
thaovt91 <vothao.tin@gmail.com>
2021-09-10 10:54:17 +0700

    up

--- Modul02/src/Week2/Array/CelsiusFahrenheit.java
+++ Modul02/src/Week2/Array/CelsiusFahrenheit.java
@@ -3,17 +3,19 @@ package Week2.Array;
 import java.util.Scanner;
 
 public class CelsiusFahrenheit {
-    public static double celsiusToFahrenheit(double c){
+    public static double celsiusToFahrenheit(double c) {
         return (9.0 / 5) * c + 32;
     }
-    public static double fahrenheitToCelsius(double f){
-        return (f- 32) *5 / 9.0;
+
+    public static double fahrenheitToCelsius(double f) {
+        return (f - 32) * 5 / 9.0;
     }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in) ;
-        double c ;
-        double f ;
-        int choice  ;
+        Scanner scanner = new Scanner(System.in);
+        double c;
+        double f;
+        int choice;
         do {
             System.out.println("Menu.");
             System.out.println("1. Fahrenheit to Celsius");
@@ -25,13 +27,13 @@ public class CelsiusFahrenheit {
             switch (choice) {
                 case 1:
                     System.out.print("Input F : ");
-                    f = scanner.nextDouble() ;
-                    System.out.println(f + " F = " +fahrenheitToCelsius(f) + " C");
+                    f = scanner.nextDouble();
+                    System.out.println(f + " F = " + fahrenheitToCelsius(f) + " C");
                     break;
                 case 2:
                     System.out.print("Input C : ");
-                    c = scanner.nextDouble() ;
-                    System.out.println(c + " C = "+celsiusToFahrenheit(c) +" F" );
+                    c = scanner.nextDouble();
+                    System.out.println(c + " C = " + celsiusToFahrenheit(c) + " F");
                     break;
                 case 0:
                     System.exit(0);
