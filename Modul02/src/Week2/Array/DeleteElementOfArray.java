edcadac0461ba32b33c0643a80723824aea9bf27
thaovt91 <vothao.tin@gmail.com>
2021-09-13 08:51:01 +0700

    up

--- Modul02/src/Week2/Array/DeleteElementOfArray.java
+++ Modul02/src/Week2/Array/DeleteElementOfArray.java
@@ -6,49 +6,50 @@ import java.util.Scanner;
 public class DeleteElementOfArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int size ;
+        int size;
         do {
             System.out.print("Nhập độ dài của mảng : ");
-            size = scanner.nextInt() ;
-            if(size > 20) {
+            size = scanner.nextInt();
+            if (size > 20) {
                 System.out.println("Độ dài của mảng phải nhở hơn  20! ");
             }
-        }while (size > 20);
-        int[]arr = new int [size] ;
-        for (int i = 0 ; i < size ; i++){
+        } while (size > 20);
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
             System.out.print("Phân tử " + i + " của mảng : ");
-            arr[i] = scanner.nextInt() ;
+            arr[i] = scanner.nextInt();
         }
         System.out.print("Nhập giá trị cần xét trong mảng : ");
-        int a = scanner.nextInt() ;
+        int a = scanner.nextInt();
         int count = 0;
-        while (findOfArray(a , arr) != -1){
+        while (findOfArray(a, arr) != -1) {
             count++;
-            int index = findOfArray(a , arr) ;
-            for (int i = index ; i <arr.length - count ;i++ ){
-                arr[i] = arr[i+1];
+            int index = findOfArray(a, arr);
+            for (int i = index; i < arr.length - count; i++) {
+                arr[i] = arr[i + 1];
             }
-            arr[arr.length - count] = 0 ;
+            arr[arr.length - count] = 0;
         }
-        if(count != 0){
-            System.out.println("Có '" + count + "' giá trị '" + a +"' trong mảng !");
+        if (count != 0) {
+            System.out.println("Có '" + count + "' giá trị '" + a + "' trong mảng !");
             System.out.print("Mảng sao khi xóa là : ");
-        }else {
-            System.out.println("Không giá trị '" + a +"' trong mảng !");
+        } else {
+            System.out.println("Không giá trị '" + a + "' trong mảng !");
             System.out.print("Mảng là : ");
         }
-        for ( int j = 0; j < arr.length ; j++){
+        for (int j = 0; j < arr.length; j++) {
             System.out.print(arr[j] + "  ");
         }
     }
-    public static int findOfArray(int a , int[]array){
-        int index = -1 ;
-        for (int i = 0; i < array.length ;i++){
-            if (a == array[i]){
-                index = i ;
+
+    public static int findOfArray(int a, int[] array) {
+        int index = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (a == array[i]) {
+                index = i;
                 break;
             }
         }
-        return index ;
+        return index;
     }
 }
