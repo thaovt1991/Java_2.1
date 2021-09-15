5dcf7f7cc7bd25c80c89b145edb54188676cee88
thaovt91 <vothao.tin@gmail.com>
2021-09-15 16:59:07 +0700

    up

--- Modul02/src/Week2/inheritance/ex_point_m/MoveablePointTest.java
+++ Modul02/src/Week2/inheritance/ex_point_m/MoveablePointTest.java
@@ -4,18 +4,19 @@ public class MoveablePointTest {
     public static void main(String[] args) {
         MoveablePoint moveable = new MoveablePoint();
         System.out.println(moveable);
-        MoveablePoint m2 = new MoveablePoint(5,7,5,7);
+        MoveablePoint m2 = new MoveablePoint(5.6f,7.3f ,5,7);
+        System.out.println(m2);
+        m2.Move();
+        m2.Move();
+        m2.Move();
+        m2.Move();
+        m2.Move();
+        System.out.print("After move : ");
         System.out.println(m2);
         MoveablePoint m3 = new MoveablePoint(7,10);
         System.out.println(m3);
         m3.Move();
+        System.out.print("After move : ");
         System.out.println(m3);
-        m2.Move();
-        m2.Move();
-        m2.Move();
-        m2.Move();
-        m2.Move();
-        System.out.println(m2);
-
     }
 }
