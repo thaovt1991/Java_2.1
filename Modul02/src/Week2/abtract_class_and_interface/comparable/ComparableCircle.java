234ae4db2f3d04e658a02c481bde2023dd9c882e
thaovt91 <vothao.tin@gmail.com>
2021-09-16 09:19:03 +0700

    up

---
+++ Modul02/src/Week2/abtract_class_and_interface/comparable/ComparableCircle.java
@@ -0,0 +1,27 @@
+package Week2.abtract_class_and_interface.comparable;
+
+import  Week2.inheritance.geometry.Circle;
+//import java.util.Comparator ;
+
+public class ComparableCircle extends Circle implements Comparable<ComparableCircle>{
+
+    @Override
+    public int compareTo(ComparableCircle obj) {
+        if (getRadius() > obj.getRadius()) return 1;
+        else if (getRadius() < obj.getRadius()) return -1;
+        else return 1;
+    }
+
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(double radius, String color, boolean filled) {
+        super(radius, color, filled);
+    }
+
+
+}
