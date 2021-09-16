883d8514e5fd6339d9e085eeefbbf1f3391bf2d7
thaovt91 <vothao.tin@gmail.com>
2021-09-16 20:18:29 +0700

    up

---
+++ Modul02/src/week3/clean_code_and_refactoring/refactoring_method/CyclinderTest.java
@@ -0,0 +1,8 @@
+package week3.clean_code_and_refactoring.refactoring_method;
+
+public class CyclinderTest {
+    public static void main(String[] args) {
+
+        System.out.println("volume =  "+ Cylinder.getVolume(3,5) );
+    }
+}
