731b81573c2f4c391bb84c055c59074171a8c982
thaovt91 <vothao.tin@gmail.com>
2021-09-22 16:15:43 +0700

    up

--- Modul02/src/week3/dsa_stack_queue/my_stack/MyStack.java
+++ Modul02/src/week3/dsa_stack_queue/my_stack/MyStack.java
@@ -1,5 +1,49 @@
 package week3.dsa_stack_queue.my_stack;
 
 public class MyStack {
-    
+    private int[] arr;
+    private int size;
+    private int index = 0;
+
+    public MyStack(int size) {
+        this.size = size;
+        arr = new int[size];
+    }
+
+    public void push(int element) {
+        if (isFull()) {
+            throw new StackOverflowError("Stack is full");
+        } else {
+            arr[index] = element;
+            index++;
+        }
+    }
+
+
+    public int pop() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Stack is null");
+        } else
+            return arr[--index];
+    }
+
+    public int size() {
+        return index;
+    }
+
+    public boolean isEmpty() {
+        if (index == 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean isFull() {
+        if (index == size) {
+            return true;
+        }
+        return false;
+    }
+
 }
