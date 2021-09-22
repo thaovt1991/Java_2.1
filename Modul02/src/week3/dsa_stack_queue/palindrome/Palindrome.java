fd324d5688a991c57225fce8af407cbdc02e4c79
thaovt91 <vothao.tin@gmail.com>
2021-09-22 20:45:41 +0700

    uppalindrome

---
+++ Modul02/src/week3/dsa_stack_queue/palindrome/Palindrome.java
@@ -0,0 +1,35 @@
+package week3.dsa_stack_queue.palindrome;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        LinkedList stack = new LinkedList();
+        Queue queue = new Queue();
+        char c;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("String need test is : ");
+        String str = scanner.nextLine();
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(i);
+            stack.push(c);
+            queue.enQueue(c);
+        }
+        String stringStack="" ;
+        String stringQueue ="" ;
+        while (!stack.isEmpty()){
+            stringStack += stack.pop() ;
+        }
+        while (!queue.isEmpty()){
+            stringQueue += queue.deQueue();
+        }
+
+       // if (stringStack.equals(str)){
+         if (stringStack.equals(stringQueue))  {
+            System.out.println("is Palindrome !");
+        }else System.out.println("Not is Palindrome !");
+    }
+}
+
