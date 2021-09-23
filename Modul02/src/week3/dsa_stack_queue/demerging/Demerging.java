020e8af41d671933273411e1e1d09f6b81922a8f
thaovt91 <vothao.tin@gmail.com>
2021-09-23 08:51:30 +0700

    up

--- Modul02/src/week3/dsa_stack_queue/demerging/Demerging.java
+++ Modul02/src/week3/dsa_stack_queue/demerging/Demerging.java
@@ -15,10 +15,10 @@ public class Demerging {
         int i = 1;
         while (i <= num) {
             Scanner scanner = new Scanner(System.in);
-            System.out.print(" Ho va ten  " + i + ": ");
+            System.out.print(" Ho va ten nv" + i + ": ");
             name = scanner.nextLine();
             do {
-            System.out.print(" Gioi tinh " + i + ": ");
+            System.out.print(" Gioi tinh nv" + i + ": ");
             gender = scanner.nextLine();
                 if((!gender.equals("NU") ) && (!gender.equals("NAM") )){
                     System.out.println("Nhap lai gioi tinh !");
@@ -27,11 +27,11 @@ public class Demerging {
             while ((!gender.equals("NU")) && (!gender.equals("NAM")));
 
             Scanner integer = new Scanner(System.in);
-            System.out.print(" Ngay sinh " + i + ": ");
+            System.out.print(" Ngay sinh nv" + i + ": ");
             day = integer.nextInt();
-            System.out.print(" Thang sinh " + i + ": ");
+            System.out.print(" Thang sinh nv" + i + ": ");
             month = integer.nextInt();
-            System.out.print(" Nam sinh " + i + ": ");
+            System.out.print(" Nam sinh nv" + i + ": ");
             year = integer.nextInt();
             if (gender.equals("NU")) {
                 QueueNu.enQueue(name, gender, day, month, year);
