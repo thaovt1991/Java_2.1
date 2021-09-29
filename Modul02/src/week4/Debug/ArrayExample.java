5805244aa15318687999521f4058971d75811efa
thaovt91 <vothao.tin@gmail.com>
2021-09-29 17:03:56 +0700

    up

---
+++ Modul02/src/week4/Debug/ArrayExample.java
@@ -0,0 +1,30 @@
+package week4.Debug;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayExample {
+    public Integer[] createRandom(){
+        Random rd= new Random() ;
+        Integer[]arr = new Integer[100];
+        System.out.println("The list of element of array !");
+        for(int i = 0; i < 100; i++){
+            arr[i] = rd.nextInt(100) ;//(int)(Math.random()*100);
+            System.out.print(arr[i] + " ");
+        }
+        return arr ;
+    }
+
+    public static void main(String[] args) {
+        ArrayExample arrayExample = new ArrayExample();
+        Integer[] arr = arrayExample.createRandom();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n Please enter the index of any element : ");
+        int x = scanner.nextInt();
+        try {
+            System.out.println("The value of " + x + " is " + arr[x]);
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("The index exceeds the limit of the array");
+        }
+    }
+}
